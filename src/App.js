5008c2fbfe3ff8997cbdc96fae1b1d563bb21a98
Itzam <fernandonovca@gmail.com>
2021-10-04 20:10:27 -0500

    npx create-react-app react-app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+const componente = () => {
+  return (
+    <>
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+    </>
+  );
+};
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
